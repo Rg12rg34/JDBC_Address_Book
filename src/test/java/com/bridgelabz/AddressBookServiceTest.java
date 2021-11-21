d225374971fbfccf4effaa2a6b6645c7e0150a62
Rg12rg34 <rahulguptamca1@gmail.com>
2021-11-21 07:46:20 +0530

    code to Update the Database using Prepared statement

--- src/test/java/com/bridgelabz/AddressBookServiceTest.java
+++ src/test/java/com/bridgelabz/AddressBookServiceTest.java
@@ -1,14 +1,27 @@
 package com.bridgelabz;
 
+import java.util.List;
+
+import org.junit.Assert;
 import org.junit.Test;
 
-import java.sql.SQLException;
-
+import com.addressbook.AddressBookDBService;
+import com.addressbook.PersonInformation;
 public class AddressBookServiceTest {
+    DatabaseConnection addressbookservice = new DatabaseConnection();
+    @Test
+    public void AddressBookDataRetrievedDFromDB_MatchCount() throws Exception {
+
+
+        List<PersonInformation> list = addressbookservice.getDataFromDB();
+
+        int actual_size = 3;
+        Assert.assertEquals(actual_size , list.size());
+    }
 
     @Test
-    public void addressBook_GetConnection() throws SQLException {
-        DatabaseConnection databaseConnection = new DatabaseConnection();
-        databaseConnection.getConnection();
+    public  void UpdateExistingRecord() throws Exception {
+        addressbookservice.updateExistingTable();
+
     }
 }
